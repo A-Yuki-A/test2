ee5832edb1debf6217266f836be524879e934850
A-Yuki-A <146058288+A-Yuki-A@users.noreply.github.com>
2023-10-29 17:44:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,46 @@
-
+code = """
 import streamlit as st
 
 def binary_to_decimal(binary_num):
-    decimal_num = int(binary_num, 2)
-    return decimal_num
+    try:
+        decimal_num = int(binary_num, 2)
+        return decimal_num
+    except ValueError:
+        return None
+
+def decimal_to_binary(decimal_num):
+    try:
+        binary_num = bin(decimal_num)[2:]
+        return binary_num
+    except ValueError:
+        return None
 
 def main():
-    st.title("Binary to Decimal Converter")
+    st.title("2進数と10進数の相互変換アプリ")
 
-    binary_input = st.text_input("Enter a binary number:", "1001")
-    if st.button("Convert"):
-        try:
-            decimal_output = binary_to_decimal(binary_input)
-            st.success(f"The decimal equivalent is {decimal_output}")
-        except ValueError:
-            st.error("Please enter a valid binary number.")
+    conversion_option = st.selectbox("変換オプション", ("2進数から10進数へ", "10進数から2進数へ"))
 
-if __name__ == "__main__":
-    main()
+    if conversion_option == "2進数から10進数へ":
+        binary_input = st.text_input("2進数を入力してください:", "1001")
+        if st.button("変換"):
+            result = binary_to_decimal(binary_input)
+            if result is not None:
+                st.success(f"10進数の値は {result} です")
+            else:
+                st.error("有効な2進数を入力してください。")
 
-def main():
-    st.title("My Streamlit App")
-    st.write("Welcome to my first Streamlit app!")
+    elif conversion_option == "10進数から2進数へ":
+        decimal_input = st.text_input("10進数を入力してください:", "9")
+        if st.button("変換"):
+            try:
+                decimal_input = int(decimal_input)
+                result = decimal_to_binary(decimal_input)
+                if result is not None:
+                    st.success(f"2進数の値は {result} です")
+                else:
+                    st.error("有効な10進数を入力してください。")
+            except ValueError:
+                st.error("有効な10進数を入力してください。")
 
 if __name__ == "__main__":
     main()
